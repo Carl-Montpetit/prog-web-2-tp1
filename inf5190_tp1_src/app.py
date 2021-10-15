47b048343e161440481a027ceba3da50473e7e92
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2021-10-15 02:46:40 -0400

    feat, chore: Added features, files, functions and more...✨♻️

--- inf5190_tp1_src/app.py
+++ inf5190_tp1_src/app.py
@@ -1,21 +1,32 @@
 import os
+from database import Database
 from flask import Flask, render_template, request, redirect, url_for, g, flash
-from flask_bootstrap import Bootstrap
-# from database import database
 from datetime import datetime
 
 # Initialize flask application
 app = Flask(__name__, static_folder="static",
             template_folder="templates", static_url_path="")
 app.secret_key = "Carl.M"
-Bootstrap(app)
 
 
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         g._database = Database()
-#     return g._database
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        g._database = Database()
+    return g._database
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.disconnect()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
 
 # DATETIME UTILS
 today = datetime.now().strftime("%Y-%m-%d")
---
+++ inf5190_tp1_src/database/articles.sql
@@ -0,0 +1,11 @@
+CREATE TABLE article (
+  id integer primary key,
+  titre varchar(100),
+  identifiant varchar(50),
+  auteur varchar(100),
+  date_publication TEXT,
+  paragraphe varchar(500)
+);
+
+INSERT INTO article(titre, identifiant, auteur, date_publication, paragraphe) 
+VALUES('Article 1', 1, 'Carl', "2021-10-14", "Ceci est un article que j'ai écris pour vous.");
